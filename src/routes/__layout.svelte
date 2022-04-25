296e000124ee9cbc8d5ffd1cc6e0b3b342d81919
bangnokia <bangnokia@gmail.com>
2022-04-26 00:00:21 +0700

    add tailwind css

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import '../app.css'
+</script>
+
+<main class="w-screen h-screen">
+	<div class="flex flex-wrap w-full h-full">
+		<nav class="flex flex-col bg-gray-500">
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/settings">Settings</a>
+		</nav>
+        <div>
+	        <slot />
+        </div>
+	</div>
+</main>
