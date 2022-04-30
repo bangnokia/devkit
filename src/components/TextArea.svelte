2f348f2017f07e204d6c93ddf2c4a5722b65de52
bangnokia <bangnokia@gmail.com>
2022-04-30 22:00:39 +0700

    work in progress

--- src/components/TextArea.svelte
+++ src/components/TextArea.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    export let label = '';
-    export let value;
+	export let label = '';
+	export let value: string;
+	export let id = '';
 
-    let className = '';
-    export {className as class};
-
+	let className = '';
+	export { className as class };
 </script>
 
 <div class="flex flex-col">
-    <label>{label}</label>
-    <textarea bind:value={value} class={`p-3 border border-slate-400 rounded ${className}`}></textarea>
+	<label for={id}>{label}</label>
+	<textarea {id} bind:value class={`p-3 border border-slate-400 rounded ${className}`} />
 </div>
