437b95a0edacad96c1299e459fb9777c3434355d
bangnokia <bangnokia@gmail.com>
2022-05-28 11:56:41 +0700

    work in progress

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Card from '../components/ui/Card.svelte';
+</script>
+
+<Card name="Devkits.app">Welcome to Devkits.app</Card>
