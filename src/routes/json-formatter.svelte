e7f6c5fc27af4fc70ad8e26065ead8b531fa81e5
bangnokia <bangnokia@gmail.com>
2022-06-02 17:56:34 +0700

    auto focus input textarea onMount

--- src/routes/json-formatter.svelte
+++ src/routes/json-formatter.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import Button from '../components/Button.svelte';
-	import TextArea from '../components/TextArea.svelte';
+	import Textarea from '../components/Textarea.svelte';
 	import Card from '../components/ui/Card.svelte';
-    import Editor from "../components/Editor.svelte";
+	import Editor from '../components/Editor.svelte';
+	import { onMount } from 'svelte';
 
-	let input = '{"name":"daudau","age":32,"address":"19 Nguyen Trai"}';
+	let input = '';
 	let output = '';
+	let inputElement: HTMLElement;
+
+	onMount(() => {
+		inputElement.focus();
+	});
+
 	function format() {
 		try {
 			output = JSON.stringify(JSON.parse(input), null, 2);
@@ -20,12 +27,12 @@
 <Card name="Json formatter">
 	<div class="grid grid-cols-2 gap-5 h-full pb-5">
 		<div class="grow flex-1 flex">
-			<TextArea label="Input" class="grow" bind:value={input}>
+			<Textarea label="Input" class="grow" bind:value={input} bind:this={inputElement}>
 				<div slot="toolbar">
 					<Button on:click={format}>Format</Button>
 				</div>
-			</TextArea>
+			</Textarea>
 		</div>
-        <Editor label="Output" bind:value={output} canCopy/>
+		<Editor label="Output" bind:value={output} canCopy />
 	</div>
 </Card>
